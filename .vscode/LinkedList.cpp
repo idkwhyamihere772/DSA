ed42373732b0c74075b5bf19d14c7c16155f153a
idkwhyamihere772 <kgfb7777@gmail.com>
2025-07-08 22:08:02 +0530

    Linked List

---
+++ .vscode/LinkedList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+//traversal in Linked List
+void trav(struct Node * head) {
+    struct Node * ptr = head;
+    while (ptr != NULL) {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+}
+int main() {
+    struct Node * head,* second, * third, * fourth;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    //Linking Head and Second Node
+    head-> data = 1;
+    head-> next = second;
+
+    //Linking Second and Third Node
+    second->data = 2;
+    second->next = third;
+    //Linking Third and Fourth Node
+    third->data = 3;
+    third->next = fourth;
+    //Linking Fourth Node to NULL
+    fourth->data = 4;
+    fourth->next = NULL;
+    trav(head);
+
+
+
+    return 0;
+}
