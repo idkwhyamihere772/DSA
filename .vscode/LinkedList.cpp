2a8ec6f3d2da2d6b7cf1067eb48b6be7f0949385
idkwhyamihere772 <kgfb7777@gmail.com>
2025-07-09 20:25:24 +0530

    updated Linked List

--- .vscode/LinkedList.cpp
+++ .vscode/LinkedList.cpp
@@ -1,47 +1,51 @@
 #include <iostream>
-#include <string>
 #include <cstdlib>
-
+#include <string>
 
 using namespace std;
 
 struct Node{
-    int data;
-    struct Node * next;
+	string data;
+	struct Node * next;
 };
 
-//traversal in Linked List
-void trav(struct Node * head) {
-    struct Node * ptr = head;
-    while (ptr != NULL) {
-        cout << ptr->data << " ";
-        ptr = ptr->next;
-    }
+void trav(struct Node * ptr){
+	while(ptr != NULL){
+		cout<<ptr -> data;
+		ptr = ptr -> next;
+	}
 }
-int main() {
-    struct Node * head,* second, * third, * fourth;
-    head = (struct Node *)malloc(sizeof(struct Node));
-    second = (struct Node *)malloc(sizeof(struct Node));
-    third = (struct Node *)malloc(sizeof(struct Node));
-    fourth = (struct Node *)malloc(sizeof(struct Node));
+int main(){
+	struct Node * head;
+	struct Node * second;
+	struct Node * third;
+	struct Node * fourth;
+	
+	// head = (struct Node *)malloc(sizeof(struct Node));
+	// second = (struct Node *)malloc(sizeof(struct Node));
+	// third = (struct Node *)malloc(sizeof(struct Node));
+	// fourth= (struct Node *)malloc(sizeof(struct Node));
 
-    //Linking Head and Second Node
-    head-> data = 11;
-    head-> next = second;
+    // we would use new instead of malloc in C++ because malloc is a C function abd doesnt call constructors
 
-    //Linking Second and Third Node
-    second->data = 21;
-    second->next = third;
-    //Linking Third and Fourth Node
-    third->data = 33;
-    third->next = fourth;
-    //Linking Fourth Node to NULL
-    fourth->data = 43;
-    fourth->next = NULL;
-    trav(head);
-    //pending updates
-
-
-
-    return 0;
+	head = new Node;
+	second = new Node;
+	third = new Node;
+	fourth = new Node;
+	
+	head -> data = "First Node -> ";
+	head -> next = second;
+	
+	second -> data = "Second Node -> ";
+    second -> next = third;
+	
+	third -> data = "Third Node -> ";
+	third -> next = fourth;
+	
+	fourth -> data = "fourth Node -> NULL";
+	fourth -> next = NULL;
+	
+	trav(head);
+	
+	
 }
